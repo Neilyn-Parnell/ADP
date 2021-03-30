3632609186e6d8821e30f405c711d4fef7d4dade
Neilyn-Parnell <neilynparnell@gmail.com>
2021-03-30 14:17:00 +0200

    FIRST COMMIT

---
+++ src/za/ac/cput/GITproject/branch218132425.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.GITproject;
+
+/**
+ *
+ * @author Neilyn Parnell
+ */
+public class branch218132425 {
+    public static int sum(final int a , final int b){
+    return a+b;
+    }
+}
